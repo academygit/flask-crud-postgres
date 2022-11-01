f4b44cd5d4d5a846b4fac2496b5b634741b2c0cd
Samu Kauppinen <samu.kauppinen@digitalentconsulting.fi>
2022-11-01 11:48:40 +0200

    project initialized

---
+++ src/data/queries.py
@@ -0,0 +1,17 @@
+import psycopg2
+from config import config
+
+con = None
+try:
+    con = psycopg2.connect(**config())
+    cursor = con.cursor()
+    SQL = 'SELECT *FROM person;'
+    cursor.execute(SQL)
+    row = cursor.fetchone()
+    print(row)
+    cursor.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if con is not None:
+        con.close()
